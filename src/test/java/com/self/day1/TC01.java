a57f9d14d207ed02f83a15a628c1f7b274002787
nurveda <utoglunurr@gmail.com>
2021-09-24 21:39:47 +0300

    added gitignore file and tc01 started

--- src/test/java/com/self/day1/TC01.java
+++ src/test/java/com/self/day1/TC01.java
@@ -1,2 +1,7 @@
-package com.self.day1;public class TC01 {
+package com.self.day1;
+
+public class TC01 {
+    public static void main(String[] args) {
+        System.out.println("nur started");
+    }
 }
